dab1038e3df43a2febcaefb3c56bf7f1ee91d7bd
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-01-05 17:01:00 +0000

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,12 +28,101 @@ export default function HomePage() {
         <h2 className="mb-4 text-2xl font-bold">About Me</h2>
         <p className="leading-relaxed">
           Hello! I&apos;m Reece. I&apos;m a PhD student at the University of
-          Oxford, where I&apos;m a member of the Oxford Robotics Institute.
-          I&apos;m interested in the intersection of machine learning and
-          robotics, and I&apos;m currently working on applying diffusion models
-          to robotic locomotion and manipulation. In my spare time I enjoy
-          powerlifting, travelling, and playing chess.
+          Oxford within Robotics Institute. I&apos;m interested in the
+          intersection of machine learning and robotics, and I&apos;m currently
+          working on applying diffusion models to quadrupedal locomotion and
+          manipulation. In addition to my PhD I also do freelance web
+          development and have experience implementing and deploying full-stack
+          applications for enterprise clients. In my spare time I enjoy
+          powerlifting, travelling, reading, and playing chess. Feel free to get
+          in touch at any of the contacts below if you&apos;d like to chat!
         </p>
+      </section>
+
+      <section className="mb-12">
+        <h2 className="mb-4 text-2xl font-bold">Work Experience</h2>
+        <div className="space-y-4">
+          <div>
+            <h3 className="text-lg font-semibold">Freelance Web Developer</h3>
+            <p className="text-sm text-gray-600">December 2024 - Present</p>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold">
+              Undergraduate Admissions Interviewer - University of Oxford
+            </h3>
+            <p className="text-sm text-gray-600">December 2024</p>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold">
+              Undergraduate Tutor - University of Oxford
+            </h3>
+            <p className="text-sm text-gray-600">October 2023 - Present</p>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold">Private Tutor</h3>
+            <p className="text-sm text-gray-600">August 2019 - October 2024</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="mb-12">
+        <h2 className="mb-4 text-2xl font-bold">Education</h2>
+        <div className="space-y-4">
+          <div>
+            <h3 className="text-lg font-semibold">
+              PhD in AI & Robotics - University of Oxford
+            </h3>
+            <p className="text-sm text-gray-600">Oxford Robotics Institute</p>
+            <p className="text-sm text-gray-600">October 2022 - Present</p>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold">
+              MEng Engineering Science, 1st class - University of Oxford
+            </h3>
+            <p className="text-sm text-gray-600">October 2018 - June 2022</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="mb-12">
+        <h2 className="mb-4 text-2xl font-bold">Skills</h2>
+        <div className="space-y-4">
+          <div>
+            <h3 className="text-lg font-semibold">AI & Robotics</h3>
+            <ul className="list-disc pl-4">
+              <li>
+                Extensive experience researching, developing, and deploying
+                maching learning models for robotics applications using PyTorch
+                and performing robotics simulation with Isaac Sim.
+              </li>
+              <li>
+                Proficiency using ROS and real-time C++ for embedded systems,
+                with particular focus on implementing ML controllers for
+                quadrupedal robots.
+              </li>
+              <li>
+                Subject matter expert on diffusion models, including their
+                application to robotics and control systems with a paper
+                submitted to ICRA 2025 currently under review.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold">Web Development</h3>
+            <ul className="list-disc pl-4">
+              <li>
+                Full-stack developer with expertise in modern
+                JavaScript/TypeScript frameworks including React, Next.js, and
+                Astro.
+              </li>
+              <li>
+                Strong background in developing enterprise-level applications,
+                from gathering client requirements to designing and deploying
+                scalable and responsive systems.
+              </li>
+            </ul>
+          </div>
+        </div>
       </section>
 
       <section>
@@ -46,7 +135,6 @@ export default function HomePage() {
             <Mail className="mr-2 h-6 w-6" />
             <span>reeceo@robots.ox.ac.uk</span>
           </a>
-
           <a
             href="https://github.com/reeceomahoney"
             className="flex items-center hover:text-blue-500"
@@ -54,7 +142,6 @@ export default function HomePage() {
             <Github className="mr-2 h-6 w-6" />
             <span>github.com/reeceomahoney</span>
           </a>
-
           <a
             href="https://www.linkedin.com/in/reece-omahoney/"
             className="flex items-center hover:text-blue-500"
