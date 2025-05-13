47a23cc35a5d4aa0525b15c708aba2db5c699677
Reece O'Mahoney <reece.omahoney3@gmail.com>
2025-05-13 12:37:31 +0100

    add flow planner page

--- src/app/publications/flow_planner/page.tsx
+++ src/app/publications/flow_planner/page.tsx
@@ -8,14 +8,15 @@ export default function Page() {
           Improving Trajectory Stitching with Flow Models
         </h1>
         <p className="mb-2 text-lg md:text-xl">
-          Reece O&pos;Mahoney, Wanming Yu, Ioannis Havoutis</p>
+          Reece O&apos;Mahoney, Wanming Yu, Ioannis Havoutis
+        </p>
         <p>Oxford Robotics Institute, University of Oxford</p>
       </div>
 
       <div className="flex justify-center gap-4">
         <button className="rounded-3xl bg-gray-800 px-4 py-2 text-white hover:bg-gray-700 md:px-6">
           <a
-            href="https://arxiv.org/abs/2411.08832"
+            href="https://arxiv.org/abs/2505.07802"
             className="flex items-center gap-2"
           >
             <Image
@@ -29,7 +30,7 @@ export default function Page() {
         </button>
         <button className="rounded-3xl bg-gray-800 px-4 py-2 text-white hover:bg-gray-700 md:px-6">
           <a
-            href="https://github.com/reeceomahoney/locodiff"
+            href="https://github.com/reeceomahoney/flow_planning"
             className="flex items-center gap-2"
           >
             <Image
@@ -44,89 +45,157 @@ export default function Page() {
       </div>
 
       <iframe
-        src="https://www.youtube.com/embed/HCrITc8e1Zs"
+        src="https://www.youtube.com/embed/v_vLEPF8ZuU"
         className="h-[200px] w-full shadow-lg md:h-[432px]"
       ></iframe>
 
       <section className="space-y-8">
-        <h2 className="text-3xl font-semibold md:text-4xl">Abstract</h2>
+        <h2 className="text-3xl font-semibold md:text-4xl">Overview</h2>
         <p className="text-left leading-relaxed">
-          We present a diffusion-based approach to quadrupedal locomotion that
-          simultaneously addresses the limitations of learning and interpolating
-          between multiple skills (modes) and of offline adapting to new
-          locomotion behaviours after training. This is the first framework to
-          apply classifier-free guided diffusion to quadruped locomotion and
-          demonstrate its efficacy by extracting goal-conditioned behaviour from
-          an originally unlabelled dataset. We show that these capabilities are
-          compatible with a multi-skill policy and can be applied with little
-          modification and minimal compute overhead, i.e., running entirely on
-          the robot&pos;s onboard CPU. We verify the validity of our approach
-          with hardware experiments on the ANYmal quadruped platform.
+          Generative models have shown great promise as trajectory planners,
+          given their affinity to modeling complex distributions and guidable
+          inference process. Previous works have successfully applied these in
+          the context of robotic manipulation but perform poorly when the
+          required solution does not exist as a complete trajectory within the
+          training set. We identify that this is a result of being unable to
+          plan via <i>stitching</i>, and subsequently address the architectural
+          and dataset choices needed to remedy this. On top of this, we propose
+          a novel addition to the training and inference procedures to both
+          stabilize and enhance these capabilities. We demonstrate the efficacy
+          of our approach by generating plans with out of distribution boundary
+          conditions and performing obstacle avoidance on the Franka Panda in
+          simulation and on real hardware. In both of these tasks our method
+          performs significantly better than the baselines and is able to avoid
+          obstacles up to four times as large.
         </p>
       </section>
 
       <section className="space-y-8">
-        <h2 className="text-3xl font-semibold md:text-4xl">Approach</h2>
-        <Image
-          src="/method.png"
-          alt="Method diagram"
-          width={800}
-          height={600}
-          className="mx-auto w-full shadow-lg md:w-11/12"
-        />
+        <h2 className="text-3xl font-semibold md:text-4xl">Flow Planner</h2>
+        <figure>
+          <Image
+            src="/flow_overview.png"
+            alt="Stitching results"
+            width={800}
+            height={600}
+            className="mx-auto w-full shadow-lg md:w-11/12"
+          />
+          <figcaption className="pt-2">
+            Figure 1: Overview: We use a flow model with three key components: a
+            local receptive, field, dataset augmentation with out addition, and
+            trajectory splitting at train time and test time. This allows us to
+            have significant improved stitching capabilities, and more flexible
+            guided planning, leading an ability to avoid much larger obstacles
+            than previous methods.
+          </figcaption>
+        </figure>
         <p className="text-left leading-relaxed">
-          Method Overview: a) A reinforcement learning agent is pre-trained with
-          a hand crafted policy that generates reference trajectories. These are
-          collected by rolling out the policy in an environment with randomised
-          parameters for robustness. b) An embedding of the observation is
-          concatenated with separate embeddings of the diffusion timestep,
-          skill, and return. These together form the conditioning input. The
-          multi-head transformer decoder initially takes a noise vector as
-          input, applies causal self-attention, then cross-attention with the
-          conditioning and produces a partially denoised vector. This process is
-          repeated N times to produce a complete action trajectory. c) The
-          return value is randomly masked during training, allowing us to use
-          classifier-free guidance at test time. This is done by taking a
-          weighted sum of unconditional and maximum return trajectories at each
-          denoising step.
+          We propose three improvements to fix stitching. Firstly, we identify
+          that a model architecture with what we call a “local receptive field”
+          is required to avoid exposing global information. Our experiments show
+          that without this, the model is strongly biased towards merely
+          selecting whole trajectories that already exist. A local receptive
+          field instead drives the model to only optimize for local consistency
+          and thus allows for a more flexible global structure.
+        </p>
+        <p className="text-left leading-relaxed">
+          Secondly, we found that action noise addition was crucial to allowing
+          the model to “jump” between existing clips at points where they
+          overlap. This was uniquely effective when compared to other dataset
+          augmentation schemes, which we hypothesise is due to the breaking of
+          the correlation between the joint states.
+        </p>
+        <p className="text-left leading-relaxed">
+          Lastly, to both prevent mode collapse, and reduce dynamics
+          inconsistencies, we apply a technique we call trajectory splitting,
+          which differs based on when it is applied. At training time, this
+          consists of randomly mixing half-length trajectories into the training
+          batch. At inference time, this consists of partially renoising the
+          trajectory after the first inference loop, splitting it in two, and
+          then denoising each half separately.
         </p>
       </section>
 
       <section className="space-y-8">
         <h2 className="text-3xl font-semibold md:text-4xl">Results</h2>
-        <Image
-          src="/results_1.png"
-          alt="Results diagram"
-          width={800}
-          height={600}
-          className="mx-auto w-full shadow-lg md:w-11/12"
-        />
         <p className="text-left">
-          After training, the model&pos;s outputs are adjusted to recover
-          different locomotion behaviours. The table above shows the velocity
-          tracking error of policies trained with different target velocities.
-          We compare an expert model with access to the ground truth commands to
-          our model that has no access to these commands but instead aims to
-          maximise a reward function via classifier-free guidance. Our results
-          demonstrate that our method, without access to ground truth commands,
-          can produce comparable velocity tracking to a model that does by using
-          reward guidance.
+          Using the combination of these techniques we create a method called
+          Flow Planner and compare it to previous approaches in two ways.
+          Firstly, we demonstrate its stitching capabilities by successfully
+          producing plans with out of distribution boundary conditions. We
+          define the stitching error as the distance between the last generated
+          state and the boundary condition on each end of the trajectory. Tables
+          1 and 2 show we are able to produce OOD trajectories more consistently
+          than previous methods and also highlight the importance of each of our
+          modifications.
         </p>
-        <Image
-          src="/results_2.png"
-          alt="Results diagram"
-          width={800}
-          height={600}
-          className="mx-auto w-full shadow-lg md:w-11/12"
-        />
+        <figure>
+          <Image
+            src="/flow_planner_table_1.png"
+            alt="Stitching results"
+            width={800}
+            height={600}
+            className="mx-auto w-full shadow-lg md:w-11/12"
+          />
+          <figcaption className="pt-2">
+            Table 1: Effects of architecture choice on stitching error
+          </figcaption>
+        </figure>
+        <figure>
+          <Image
+            src="/flow_planner_table_2.png"
+            alt="Stitching results"
+            width={800}
+            height={600}
+            className="mx-auto w-full shadow-lg md:w-11/12"
+          />
+          <figcaption className="pt-2">
+            Table 2: Effects of Dataset augmentation on stitching error
+          </figcaption>
+        </figure>
         <p className="text-left leading-relaxed">
-          We collect separate datasets generated by walking and crawling
-          reinforcement learning policies with no transitions present between
-          the two. Our model was able to learn interpolations between these two
-          skills which were remarkably stable over the full range of velocity
-          commands in the dataset. The bottom row of the above figure shows
-          snapshots from one of these transitions when deployed on real
-          hardware.
+          Secondly, we apply it to an obstacle avoidance problem where no single
+          trajectory is a complete solution, hence also requiring stitching.
+          Flow planner is able to both plan more successfully, and avoid much
+          larger obstacles than previous methods. As shown in the figure below.
+        </p>
+
+        <figure>
+          <Image
+            src="/obstacle.png"
+            alt="Guided planning results"
+            width={800}
+            height={600}
+            className="mx-auto w-full shadow-lg md:w-11/12"
+          />
+          <figcaption className="pt-2">
+            Figure 2: Performance benchmark in an obstacle avoidance task on a
+            Franka arm in simulation. Left: Sample trajectories from obstacle
+            avoidance experiments. Right: Maximum obstacle radius. that each
+            method was able to reliably avoid.
+          </figcaption>
+        </figure>
+        <p className="text-left leading-relaxed">
+          We lastly demonstrate our methods generalisability by deploying it
+          onto real hardware.
+        </p>
+        <figure>
+          <Image
+            src="/hardware.png"
+            alt="Stitching results"
+            width={800}
+            height={600}
+            className="mx-auto w-full shadow-lg md:w-11/12"
+          />
+          <figcaption className="pt-2">
+            Figure 3: Hardware deployment, the top row shows an unguided plan
+            and the bottom a guided one.
+          </figcaption>
+        </figure>
+        <p className="text-left leading-relaxed">
+          In conclusion, we present a novel, flow matching based planner for
+          robotic manipulation that, through superior stitching abilities, is
+          able to plan more flexibly, and robustly than previous methods.
         </p>
       </section>
     </div>
